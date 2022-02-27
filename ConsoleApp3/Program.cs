c8ab5a4968eb01379f4ecca29df34323c4ebf82a
ankitsinghrana13 <ak9897432538@gmail.com>
2022-02-28 06:26:13 +0800

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace ConsoleApp3
+{
+    class program
+    {
+        static void Main(string[] arg)
+        {
+            int a,b, choice;
+            Console.WriteLine("Adding for choice one");
+            Console.WriteLine("Substaction for choice one");
+            Console.WriteLine("Multiplication for choice one");
+
+            Console.WriteLine("Enter the choice");
+            choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+
+                case 1:
+                    Console.WriteLine("you Choice Adding");
+                    Console.WriteLine("Enter the value of A");
+                    a= Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter the value of B");
+                    b= Convert.ToInt32(Console.ReadLine());
+                   
+                    Console.WriteLine("Adding="+ a+b);
+                    Console.ReadLine();
+                    break;
+
+                case 2:
+                    Console.WriteLine("you Choice ubstraction");
+                    Console.WriteLine("Enter the value of A");
+                    a = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter the value of B");
+                    b = Convert.ToInt32(Console.ReadLine());
+                
+                    Console.WriteLine("ubstration="+ a+b);
+                    Console.ReadLine();
+
+                    break;
+                
+                case 3:
+
+                    Console.WriteLine("you Choice Multiplication");
+                    Console.WriteLine("Enter the value of A");
+                    a = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter the value of B");
+                    b = Convert.ToInt32(Console.ReadLine());
+
+                  
+                    Console.WriteLine("ubstration="+ a*b);
+                    Console.ReadLine();
+
+                    break;
+
+            }
+
+
+        }
+    }
+
+}
